893d5d7aafb4419346d28fb2a8b7c313cf3f89bd
Janani Selvam <jananiselvam116@gmail.com>
2026-01-25 17:31:07 +0530

    Add files via upload

---
+++ Problem_14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   
+   int num = 10;
+   int num1 = 5;
+   int sum = num + num1;
+   printf("%d\n",sum);
+   
+   int a = 15;
+   int b = 15;
+   int sum1 = a + b;
+   printf("%d",sum1);
+    
+
+   
+    return 0;
+}
