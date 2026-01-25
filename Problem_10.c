ea114cd938f410ba57345467d333c9abaa97d6cf
Janani Selvam <jananiselvam116@gmail.com>
2026-01-25 17:05:30 +0530

    Add files via upload

---
+++ Problem_10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   
+   int num = 5;
+   int num1 = 15;
+   printf("Output:%d %d\n",num,num1);
+    
+   int num2 = 20;
+   int num3 = 40;
+   printf("Output:%d %d",num2,num3);
+   
+    return 0;
+}
