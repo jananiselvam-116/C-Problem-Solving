6ccb0bb48d9f982f75fea90e05930e21f384aa6a
Janani Selvam <jananiselvam116@gmail.com>
2026-01-25 19:27:52 +0530

    Add files via upload

---
+++ Problem_29.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   
+  int a,b;
+  scanf("%d %d",&a,&b);
+  int sum = a + b;
+  printf("%d\n",sum);
+  
+  int a1,b1;
+  scanf("%d %d",&a1,&b1);
+  int sum1 = a1 + b1;
+  printf("%d",sum1);
+   
+    return 0;
+}
