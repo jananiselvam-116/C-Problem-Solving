183f62e1dff5db286697d22a620f8a8afa9fb1bb
Janani Selvam <jananiselvam116@gmail.com>
2026-01-25 19:23:07 +0530

    Add files via upload

---
+++ Problem_28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   
+  char ch = 'A';
+  int ch1;
+  ch1 = ch;
+  printf("%d\n",ch1);
+  
+ char c = 'a';
+ int c1;
+ c1 = c;
+ printf("%d",c1);
+   
+    return 0;
+}
