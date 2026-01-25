764bf9f262cc64f218398667158056be7bf380e9
Janani Selvam <jananiselvam116@gmail.com>
2026-01-25 17:26:34 +0530

    Add files via upload

---
+++ Problem_13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   
+   int num = 10;
+   num = 20;
+   printf("%d\n",num);
+   
+   int num1 = 10;
+   num1 = 100;
+   printf("%d\n",num1);
+    
+
+   
+    return 0;
+}
