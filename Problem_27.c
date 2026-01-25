58de0cf4f0fef51834957ca1315dd72f6ceae697
Janani Selvam <jananiselvam116@gmail.com>
2026-01-25 19:15:36 +0530

    Add files via upload

---
+++ Problem_27.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+   
+  int num = 10;
+  float a;
+  a = num;
+  
+  printf("%.1f\n",a);
+  
+  
+  int num1 = 25;
+  float b;
+  b = num1;
+  printf("%.1f",b);
+  
+ 
+   
+    return 0;
+}
