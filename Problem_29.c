0b42a2ae486b229bfd01ed81847bc6a04d81bd84
Janani Selvam <jananiselvam116@gmail.com>
2026-01-25 19:30:34 +0530

    Update Problem_29.c

--- Problem_29.c
+++ Problem_29.c
@@ -11,6 +11,12 @@ int main() {
   scanf("%d %d",&a1,&b1);
   int sum1 = a1 + b1;
   printf("%d",sum1);
+
+   int a2,b2;
+  scanf("%d %d",&a2,&b2);
+  int sum1 = a2 + b2;
+  printf("%d",sum2);
    
     return 0;
+
 }
