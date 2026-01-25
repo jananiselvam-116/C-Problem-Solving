1a9fcc93764364726611f87f2283684af1acffab
Janani Selvam <jananiselvam116@gmail.com>
2026-01-25 18:14:58 +0530

    Add files via upload

---
+++ Problem_17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   
+   int num = 20;
+   int num1 = 5;
+   int sum = num / num1;
+   printf("%d\n",sum);
+   
+   int a = 20;
+   int b = 4;
+   int sum1 = a / b;
+   printf("%d",sum1);
+    
+
+   
+    return 0;
+}
