fea92a4617a56e539be7a0335d4eacac2ab23ad7
Janani Selvam <jananiselvam116@gmail.com>
2026-01-26 20:44:21 +0530

    Add files via upload

---
+++ Problem_52.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>0){
+        printf("Positive number");
+    }else if(n<0){
+        printf("Negative number");
+    }else{
+        printf("Zero");
+    }
+    
+
+    return 0;
+}
