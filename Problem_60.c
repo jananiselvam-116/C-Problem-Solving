9998309bd87778463ea3575527185fa7f6ad21c9
Janani Selvam <jananiselvam116@gmail.com>
2026-01-27 11:16:21 +0530

    Add files via upload

---
+++ Problem_60.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    scanf("%d",&age);
+    if(age>=18){
+        printf("Eligible to vote");
+    }else{
+        printf("not Eligible to vote");
+    }
+    
+
+    return 0;
+}
