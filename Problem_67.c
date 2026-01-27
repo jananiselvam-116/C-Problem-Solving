a011cb0906286c6ffa449622bac99360230ff18d
Janani Selvam <jananiselvam116@gmail.com>
2026-01-27 11:19:05 +0530

    Add files via upload

---
+++ Problem_67.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num1,num2,num3;
+    scanf("%d %d %d",&num1,&num2,&num3);
+    if(num1==num2 && num2==num3){
+         printf("Equilateral Triangle");
+    }else {
+       
+         printf("Not Equilateral Triangle");
+    }
+    
+
+    return 0;
+}
