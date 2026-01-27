2fb43c2df0e88a15a9a9687ed0aedfee8b1da697
Janani Selvam <jananiselvam116@gmail.com>
2026-01-27 11:16:40 +0530

    Add files via upload

---
+++ Problem_61.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    if(num>=100 && num<=999){
+        printf("Three-digit number");
+    }else{
+        printf("not Three-digit number");
+    }
+    
+
+    return 0;
+}
