713055b8b59a2b9e1301f5ac249e2842e497685b
Janani Selvam <jananiselvam116@gmail.com>
2026-01-28 22:49:51 +0530

    Add files via upload

---
+++ loop problem/loop_problem16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int sum = 0;
+    for (int i = 1; i <= N / 2; i++) {
+        if (N % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == N && N != 0) {
+        printf("Yes");
+    } else {
+        printf("No");
+    }
+
+    return 0;
+}
