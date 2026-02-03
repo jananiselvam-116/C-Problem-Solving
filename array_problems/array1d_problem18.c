799fed06966b0b759f8a3b4d2921bb236f0ad980
Janani Selvam <jananiselvam116@gmail.com>
2026-02-03 22:26:36 +0530

    Add files via upload

---
+++ array_problems/array1d_problem18.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int count = 1;
+    int max = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] > a[i - 1]) {
+            count++;
+        } else {
+            count = 1;
+        }
+
+        if (count > max) {
+            max = count;
+        }
+    }
+
+    printf("%d", max);
+    return 0;
+}
