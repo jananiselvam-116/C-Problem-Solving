6f34f499799f67e350c71bf35e5d55895694f8ba
Janani Selvam <jananiselvam116@gmail.com>
2026-02-03 22:23:01 +0530

    Add files via upload

---
+++ array_problems/array1d_problem14.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min = INT_MAX;
+    int found = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > 0) {
+            if (arr[i] < min) {
+                min = arr[i];
+                found = 1;
+            }
+        }
+    }
+
+    if (found) {
+        printf("%d", min);
+    } else {
+        printf("No positive");
+    }
+
+    return 0;
+}
