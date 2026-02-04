c24ab2b5fd4cdf14a2170078df73bf888d50bc35
Janani Selvam <jananiselvam116@gmail.com>
2026-02-04 23:10:04 +0530

    Add files via upload

---
+++ array_problems/array1d_problem24.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int unique = 1; 
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                unique = 0;
+                break;
+            }
+        }
+        if (unique == 0)
+            break;
+    }
+
+    if (unique)
+        printf("Yes");
+    else
+        printf("No");
+
+    return 0;
+}
