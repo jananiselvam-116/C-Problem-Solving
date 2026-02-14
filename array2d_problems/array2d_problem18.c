19b388e6870b75cca448d29835598ef7b0aa83d1
Janani Selvam <jananiselvam116@gmail.com>
2026-02-14 22:05:58 +0530

    Add files via upload

---
+++ array2d_problems/array2d_problem18.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+
+    int matrix[R][C];
+
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int maxDiff = 0;
+
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+            for(int k = 0; k < R; k++) {
+                if(k != i) {
+                    for(int l = 0; l < C; l++) {
+                        int diff = abs(matrix[i][j] - matrix[k][l]);
+                        if(diff > maxDiff) {
+                            maxDiff = diff;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    printf("%d", maxDiff);
+
+    return 0;
+}
