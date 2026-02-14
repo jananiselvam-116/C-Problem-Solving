70ed06e5dcd068d52da6d11ba5384d904f92bd91
Janani Selvam <jananiselvam116@gmail.com>
2026-02-14 22:05:29 +0530

    Add files via upload

---
+++ array2d_problems/array2d_problem17.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+
+    int matrix[R][C];
+
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int count = 0;
+
+    for(int i = 0; i < R; i++) {
+        int diagonal = matrix[i][i];
+        int sum = 0;
+
+        for(int j = 0; j < C; j++) {
+            if(j != i) {
+                sum += matrix[i][j];
+            }
+        }
+
+        if(diagonal > sum) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
