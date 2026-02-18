5042b3a44878037924a91901c1bfedcb6ddecbdd
Janani Selvam <jananiselvam116@gmail.com>
2026-02-18 22:06:16 +0530

    Add files via upload

---
+++ array2d_problems/array2d_problem24.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if(num <= 1)
+        return 0;
+    for(int i = 2; i * i <= num; i++) {
+        if(num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+
+    int matrix[R][C];
+    int count = 0;
+
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(int i = 0; i < R; i++) {
+        int allPrime = 1;
+
+        for(int j = 0; j < C; j++) {
+            if(!isPrime(matrix[i][j])) {
+                allPrime = 0;
+                break;
+            }
+        }
+
+        if(allPrime)
+            count++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
